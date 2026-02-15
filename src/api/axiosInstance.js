3ec0ce42d6794bfe6e87a55eda3b8b34f625c809
Arief Syaifuddin <asvelezer@gmail.com>
2026-02-16 01:07:19 +0700

    add env

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
+import Constants from 'expo-constants';
 
-const API_BASE_URL = 'https://spd-lab-backend-db797613f87b.herokuapp.com';
+const API_BASE_URL = Constants.expoConfig?.extra?.apiBaseUrl || 'https://spd-exam-backend-2af53d7fefb5.herokuapp.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
